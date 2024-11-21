42064de0202c6194fd78fb64a6fb171899deaa1c
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-21 09:00:31 +0100

    02 routes and views

---
+++ _02_routes_and_views/resources/views/ninjas/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel Ninjas | Home</title>
+    </head>
+    <body>
+        <h2>Currently available Laravel Ninjas</h2>
+        
+        <ul>
+            <li>Ninja 1</li>
+            <li>Ninja 2</li>
+        </ul>
+    </body>
+</html>
---
+++ _02_routes_and_views/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel Ninjas</title>
+    </head>
+    <body>
+        <h1>The Laravel Ninjas</h1>
+        <p>Click the button below to view thz list of warriors</p>
+        <a href="/ninjas">Find warriors</a>
+
+    </body>
+</html>
